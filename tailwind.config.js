69418d3022218f361027d031d0b62e1259271cd9
waeltaieb <130119649+waeltaieb@users.noreply.github.com>
2023-04-07 04:31:34 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        '128': '52rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
